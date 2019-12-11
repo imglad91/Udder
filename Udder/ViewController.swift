702c0f6c79a8f51693675b17245bc890a82abb93
Glad <gladpoenaru@icloud.com>
2019-12-10 21:51:03 -0700

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Glad Poenaru on 2019-12-10.
+//  Copyright © 2019 Glad Poenaru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
